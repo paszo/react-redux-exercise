7ead32c23d75196fd4e9c8fcd84930ecfcea8d60
paszo <paszo2008@gmail.com>
2023-06-04 07:53:39 +0200

    added redux-saga

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getUser} from "./redux/ducks/user";
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getUser());
+    }, [dispatch]);
+
+    const user = useSelector((state) => state.user.user);
+    console.log(user);
 
     const count = useSelector((state) => state.counter.count);
 
     const voters = ["Antony Sistili", "Bob Smiths", "Stephanie Foo", "Kevin Ma", "Paul Go"];
   return (
     <div className="App">
+        {user && <h1>Hello, {user.firstName} </h1>}
       <h1>Redux made easy</h1>
         <h2>Total Votes: {count}</h2>
         {voters.map(voter => (<Counter name={voter}/>))}
